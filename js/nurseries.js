62618fd6655dc9e3939d896f5c3e18749dc76b38
indiajohnson-cm <46683218+indiajohnson-cm@users.noreply.github.com>
2020-03-05 13:44:01 -0500

    Test 3

--- js/nurseries.js
+++ js/nurseries.js
@@ -76,10 +76,9 @@ var geoJson = ({
           41.6166118
         ]
       }
-    },
-	{
+    },{
     'type': 'FeatureCollection',
-    'features': [{
+    'features': {
                 'type': 'Feature',
                 'properties': {
                     'Timestamp': '10/21/2019 12:28:29',
@@ -110,13 +109,7 @@ var geoJson = ({
                     'type': 'Point',
                     'coordinates': [-81.88792299659391, 40.90153052205019]
                 }
-            }, {
-                'type': 'Feature',
-                'properties': {
-                    'Timestamp': '10/30/2019 14:24:52',
-                    'Nursery_Status': 'Existing Nursery',
-                    'Nursery': \ "Sarah's Garden\", 'State_1': 'Indiana', 'Type': 'Retail, Catalog', 'Native _%': '50', 'Native': 'No', 'Keep': 'Seed, Herbaceous', 'Grow_Type': 'Germinated from seed', 'Specialty': nan, 'Address': '1234 Main Street', 'Owner_Con': 'Sarah', 'Contact_Number': 1234567, 'Contact_Email': 'sre@clevelandmetroparks.com', 'Preferred_Contact': 'Email', 'Notes': 'This is a test, only a test', 'Questions and comments': nan, 'Zip': nan, 'location': None, 'point': None, 'Latitude': nan, 'Longitude': nan, 'elevation': nan}, 'geometry': {'type': 'Point', 'coordinates': [nan, nan]}}, {'type': 'Feature', 'properties': {'Timestamp': '12/10/2019 17:10:53', 'Nursery_Status': 'New Nursery', 'Nursery': 'Flowey', 'State_1': 'OH', 'Type': 'Wholesale, Retail, Online, Open to the Public, Appointment Only, Small/Home Business, Catalog', 'Native _%': '80', 'Native': 'Yes', 'Keep': 'Seed, Plants, Herbaceous, Ferns, Woody, Shrubs, Bare Root, Containerized Trees, B&B Trees', 'Grow_Type': 'Grown from known seed locations (can track provenance), Germinated from seed, Grown by clonal dividing', 'Specialty': 'Organic, Wetland Restoration, Native Plants', 'Address': '3900 Wildlife Way', 'Owner_Con': 'India Johnson', 'Contact_Number': 2138906234, 'Contact_Email': 'itj@clevelandmetroparks.com', 'Preferred_Contact': 'Phone', 'Notes': nan, 'Questions and comments': nan, 'Zip': 44109.0, 'location': Location(Cleveland Metroparks Zoo, 3900, Wildlife Way, Old Brooklyn, Cleveland, Cuyahoga County, Ohio, 44109, United States of America, (41.4465205, -81.7123126731083, 0.0)), 'point': (41.4465205, -81.7123126731083, 0.0), 'Latitude': 41.4465205, 'Longitude': -81.7123126731083, 'elevation': 0.0}, 'geometry': {'type': 'Point', 'coordinates': [-81.7123126731083, 41.4465205]}}, {'type': 'Feature', 'properties': {'Timestamp': '12/13/2019 15:52:16', 'Nursery_Status': 'New Nursery', 'Nursery': 'Flowers and Dollars', 'State_1': 'PA', 'Type': 'Wholesale, Retail, Online, Open to the Public, Appointment Only, Small/Home Business, Catalog', 'Native _%': '80', 'Native': 'Yes', 'Keep': 'Seed, Plants, Herbaceous, Ferns, Containerized Trees', 'Grow_Type': 'Grown from known seed locations (can track provenance), Germinated from seed, Grown from starter plugs', 'Specialty': 'Organic, Wetland Restoration, Native Plants', 'Address': '243 Pennsylvania Ave, Mill Hall', 'Owner_Con': 'India', 'Contact_Number': 2130987635, 'Contact_Email': nan, 'Preferred_Contact': 'Phone', 'Notes': nan, 'Questions and comments': nan, 'Zip': 17751.0, 'location': Location(243, Pennsylvania Avenue, Mill Hall, Clinton County, Pennsylvania, 17751, United States of America, (41.11300587309483, -77.48293300734929, 0.0)), 'point': (41.11300587309483, -77.48293300734929, 0.0), 'Latitude': 41.11300587309483, 'Longitude': -77.48293300734929, 'elevation': 0.0}, 'geometry': {'type': 'Point', 'coordinates': [-77.48293300734929, 41.11300587309483]}}]}  
-	,
+            },
     {
       "type": "Feature",
       "properties": {
