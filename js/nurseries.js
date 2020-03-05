309f493f41fe66cc282e89307a23b6acaef1a2f8
indiajohnson-cm <46683218+indiajohnson-cm@users.noreply.github.com>
2020-03-05 13:42:16 -0500

    Test 2

--- js/nurseries.js
+++ js/nurseries.js
@@ -78,46 +78,45 @@ var geoJson = ({
       }
     },
 	{
-    'type': 'Feature',
-    'properties': {
-        'Timestamp': '12/13/2019 15:52:16',
-        'Are you changing a listing or adding a new one?': 'New Nursery',
-        'Nursery Name': 'Flowers and Dollars',
-        'In what state is your nursery located? (Use Two-Letter Designation)': 'PA',
-        'Type of Nursery': 'Wholesale, Retail, Online, Open to the Public, Appointment Only, Small/Home Business, Catalog',
-        'What percent of your inventory is Native?': '80',
-        'Do you have Ohio Native Species?': 'Yes',
-        'What does your inventory consist of?': 'Seed, Plants, Herbaceous, Ferns, Containerized Trees',
-        'Are your plants...': 'Grown from known seed locations 
-            (can track provenance),
-        Germinated from seed,
-        Grown from starter plugs ', '
-        Does your nursery specialize in ...': '
-        Organic,
-        Wetland Restoration,
-        Native Plants ', '
-        Address(street, city)
-        ': '
-        243 Pennsylvania Ave,
-        Millnia Avenue,
-        Mill Hall,
-        Clinton County,
-        Pennsylvania,
-        17751,
-        United States of America,
-        (41.11300587309483, -77.48293300734929, 0.0)),
-    'point': (41.11300587309483, -77.48293300734929, 0.0),
-    'latitude': 41.11300587309483,
-    'longitude': -77.48293300734929,
-    'elevation': 0.0
-	"marker-color": "#254503",
-        "marker-size": "small",
-        "marker-symbol": "garden"   
-}, 'geometry': {
-    'type': 'Point',
-    'coordinates': [-77.48293300734929, 41.11300587309483]
-}
-},
+    'type': 'FeatureCollection',
+    'features': [{
+                'type': 'Feature',
+                'properties': {
+                    'Timestamp': '10/21/2019 12:28:29',
+                    'Nursery_Status': 'New Nursery',
+                    'Nursery': 'Plants and Posies',
+                    'State_1': 'OH',
+                    'Type': 'Wholesale, Retail, Online, Open to the Public',
+                    'Native _%': '200%',
+                    'Native': 'Yes',
+                    'Keep': 'Seed, Plants, Herbaceous, Ferns, Woody, Shrubs, Bare Root, Containerized Trees, B&B Trees',
+                    'Grow_Type': 'From known seed locations, Grown from seed',
+                    'Specialty': 'Organic, Wetland Restoration, Native Plants, Bioengineering Material',
+                    'Address': '4202 Fulton rd. ',
+                    'Owner_Con': 'India Johnson',
+                    'Contact_Number': 2169037771,
+                    'Contact_Email': 'itj3@case.edu',
+                    'Preferred_Contact': 'Phone, Email',
+                    'Notes': 'This is a test',
+                    'Questions and comments': 'Do you even host brah?',
+                    'Zip': nan,
+                    'location': Location(4202, Fulton Road, Hermanville, Canaan Township, Wayne County, Ohio, 44677, United States of America, (40.90153052205019, -81.88792299659391, 0.0)),
+                    'point': (40.90153052205019, -81.88792299659391, 0.0),
+                    'Latitude': 40.90153052205019,
+                    'Longitude': -81.88792299659391,
+                    'elevation': 0.0
+                },
+                'geometry': {
+                    'type': 'Point',
+                    'coordinates': [-81.88792299659391, 40.90153052205019]
+                }
+            }, {
+                'type': 'Feature',
+                'properties': {
+                    'Timestamp': '10/30/2019 14:24:52',
+                    'Nursery_Status': 'Existing Nursery',
+                    'Nursery': \ "Sarah's Garden\", 'State_1': 'Indiana', 'Type': 'Retail, Catalog', 'Native _%': '50', 'Native': 'No', 'Keep': 'Seed, Herbaceous', 'Grow_Type': 'Germinated from seed', 'Specialty': nan, 'Address': '1234 Main Street', 'Owner_Con': 'Sarah', 'Contact_Number': 1234567, 'Contact_Email': 'sre@clevelandmetroparks.com', 'Preferred_Contact': 'Email', 'Notes': 'This is a test, only a test', 'Questions and comments': nan, 'Zip': nan, 'location': None, 'point': None, 'Latitude': nan, 'Longitude': nan, 'elevation': nan}, 'geometry': {'type': 'Point', 'coordinates': [nan, nan]}}, {'type': 'Feature', 'properties': {'Timestamp': '12/10/2019 17:10:53', 'Nursery_Status': 'New Nursery', 'Nursery': 'Flowey', 'State_1': 'OH', 'Type': 'Wholesale, Retail, Online, Open to the Public, Appointment Only, Small/Home Business, Catalog', 'Native _%': '80', 'Native': 'Yes', 'Keep': 'Seed, Plants, Herbaceous, Ferns, Woody, Shrubs, Bare Root, Containerized Trees, B&B Trees', 'Grow_Type': 'Grown from known seed locations (can track provenance), Germinated from seed, Grown by clonal dividing', 'Specialty': 'Organic, Wetland Restoration, Native Plants', 'Address': '3900 Wildlife Way', 'Owner_Con': 'India Johnson', 'Contact_Number': 2138906234, 'Contact_Email': 'itj@clevelandmetroparks.com', 'Preferred_Contact': 'Phone', 'Notes': nan, 'Questions and comments': nan, 'Zip': 44109.0, 'location': Location(Cleveland Metroparks Zoo, 3900, Wildlife Way, Old Brooklyn, Cleveland, Cuyahoga County, Ohio, 44109, United States of America, (41.4465205, -81.7123126731083, 0.0)), 'point': (41.4465205, -81.7123126731083, 0.0), 'Latitude': 41.4465205, 'Longitude': -81.7123126731083, 'elevation': 0.0}, 'geometry': {'type': 'Point', 'coordinates': [-81.7123126731083, 41.4465205]}}, {'type': 'Feature', 'properties': {'Timestamp': '12/13/2019 15:52:16', 'Nursery_Status': 'New Nursery', 'Nursery': 'Flowers and Dollars', 'State_1': 'PA', 'Type': 'Wholesale, Retail, Online, Open to the Public, Appointment Only, Small/Home Business, Catalog', 'Native _%': '80', 'Native': 'Yes', 'Keep': 'Seed, Plants, Herbaceous, Ferns, Containerized Trees', 'Grow_Type': 'Grown from known seed locations (can track provenance), Germinated from seed, Grown from starter plugs', 'Specialty': 'Organic, Wetland Restoration, Native Plants', 'Address': '243 Pennsylvania Ave, Mill Hall', 'Owner_Con': 'India', 'Contact_Number': 2130987635, 'Contact_Email': nan, 'Preferred_Contact': 'Phone', 'Notes': nan, 'Questions and comments': nan, 'Zip': 17751.0, 'location': Location(243, Pennsylvania Avenue, Mill Hall, Clinton County, Pennsylvania, 17751, United States of America, (41.11300587309483, -77.48293300734929, 0.0)), 'point': (41.11300587309483, -77.48293300734929, 0.0), 'Latitude': 41.11300587309483, 'Longitude': -77.48293300734929, 'elevation': 0.0}, 'geometry': {'type': 'Point', 'coordinates': [-77.48293300734929, 41.11300587309483]}}]}  
+	,
     {
       "type": "Feature",
       "properties": {
