2043c39ae937246b779b371f23bb9804f8fd7dd7
indiajohnson-cm <46683218+indiajohnson-cm@users.noreply.github.com>
2020-03-12 17:17:20 -0400

    Remove marker params for fake nursery
    Naturally Potato

--- js/nurseries.js
+++ js/nurseries.js
@@ -97,9 +97,6 @@ var geoJson = ({
                 "Sales_Con": "Jan Hunter",
                 "General E": "nnn@naturallyPotato.net",
                 "Facebook": "www.naturallyPotato.net",
-                "marker-color": "#254503",
-                "marker-size": "large",
-                "marker-symbol": "garden"
             },
             "geometry": {
                 "type": "Point",
