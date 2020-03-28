89af5d1641248038e5066c0fbcb3210335a54af5
indiajohnson-cm <46683218+indiajohnson-cm@users.noreply.github.com>
2020-03-28 16:23:42 -0400

    Remove Fake Nurseries

--- js/nurseries.js
+++ js/nurseries.js
@@ -78,70 +78,7 @@ var geoJson = ({
                 ]
             }
         },
-        {
-            "type": "Feature",
-            "properties": {
-                "Nursery": "Naturally Potato",
-                "Contact_Email": "nnn@naturallyPotato.net",
-                "Contact_Number": "(414) 833-2020",
-                "State": "IN",
-                "Address": "27056 New Rd, North Liberty, IN 46554",
-                "Street": "27056 New Rd",
-                "City": "North Liberty",
-                "State_1": "IN",
-                "Zip": 46554,
-                "Keep": "forbs, ferns, grass/sedges, trees, shrubs",
-                "Type": "Order via email or call (419) 833-2020 or visit!",
-                "Latitude": 41.563662,
-                "Longitude": -86.388569,
-                "Owner_Con": "Jan Hunter",
-                "Sales_Con": "Jan Hunter",
-                "General E": "nnn@naturallyPotato.net",
-                "Facebook": "www.naturallyPotato.net",
-                "marker-color": "#254503",
-                "marker-size": "small",
-                "marker-symbol": "garden"
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    -86.388569,
-                    41.563662
-                ]
-            }
-        },
-        {
-            'type': 'Feature',
-            'properties': {
-                'Timestamp': '10/21/2019 12:28:29',
-                'Nursery_Status': 'New Nursery',
-                'Nursery': 'Plants and Posies',
-                'State_1': 'OH',
-                'Type': 'Wholesale, Retail, Online, Open to the Public',
-                'Native _%': '200%',
-                'Native': 'Yes',
-                'Keep': 'Seed, Plants, Herbaceous, Ferns, Woody, Shrubs, Bare Root, Containerized Trees, B&B Trees',
-                'Grow_Type': 'From known seed locations, Grown from seed',
-                'Specialty': 'Organic, Wetland Restoration, Native Plants, Bioengineering Material',
-                'Address': '4202 Fulton rd. ',
-                'Owner_Con': 'India Johnson',
-                'Contact_Number': 2169037771,
-                'Contact_Email': 'itj3@case.edu',
-                'Preferred_Contact': 'Phone, Email',
-                'Notes': 'This is a test',
-                'Questions and comments': 'Do you even host brah?',
-                'Zip': 44144,
-                'point': (40.90153052205019, -81.88792299659391, 0.0),
-                'Latitude': 40.90153052205019,
-                'Longitude': -81.88792299659391,
-                'elevation': 0.0
-            },
-            'geometry': {
-                'type': 'Point',
-                'coordinates': [-81.88792299659391, 40.90153052205019]
-            }
-        },
-        {
+        
             "type": "Feature",
             "properties": {
                 "Nursery": "Cold Stream Farm",
