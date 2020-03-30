d7f685ff65f01e5a4ea58f8fb456b8bd7353e144
indiajohnson-cm <46683218+indiajohnson-cm@users.noreply.github.com>
2020-03-29 20:45:32 -0400

    Updated Nursery list w/nan to null

--- js/nurseries.js
+++ js/nurseries.js
@@ -81,7 +81,390 @@ var geoJson = ({
                 41.6166118
             ]
         }
-    },  
+    },
+    {
+        'type': 'Feature',
+        'properties': {
+            'Timestamp': '12/13/2019 15: 52: 16',
+            'Nursery_Status': 'New Nursery',
+            'Nursery': 'Flowers and Dollars',
+            'State_1': 'PA',
+            'Type': 'Wholesale, Retail, Online, Open to the Public, Appointment Only, Small/Home Business, Catalog',
+            'Native _%': '80',
+            'Native': 'Yes',
+            'Keep': 'Seed, Plants, Herbaceous, Ferns, Containerized Trees',
+            'Grow_Type': 'Grown from known seed locations (can track prove"null"ce), Germinated from seed, Grown from starter plugs',
+            'Specialty': 'Organic, Wetland Restoration, Native Plants',
+            'Address': '243 Pennsylvania Ave, Mill Hall',
+            'Owner_Con': 'India',
+            'Contact_Number': '2130987635',
+            'Contact_Email': "null",
+            'Preferred_Contact': 'Phone',
+            'Notes': "null",
+            'Questions and comments': "null",
+            'Zip': 17751.0,
+            'Facebook': "null",
+            'Test': "null",
+            'point': (41.11300587309483,
+                -77.48293300734929,
+                0.0),
+            'Latitude': 41.11300587309483,
+            'Longitude': -77.48293300734929,
+            'elevation': 0.0
+        },
+        'geometry': {
+            'type': 'Point',
+            'coordinates': [
+                -77.48293300734929,
+                41.11300587309483
+            ]
+        }
+    },
+    {
+        'type': 'Feature',
+        'properties': {
+            'Timestamp': '1/23/2020 10: 55: 25',
+            'Nursery_Status': 'New Nursery',
+            'Nursery': 'Dropseed Native Plant Nursery',
+            'State_1': 'KY',
+            'Type': 'Wholesale, Retail, Online, Open to the Public, Small/Home Business',
+            'Native _%': '100',
+            'Native': 'Yes',
+            'Keep': 'Seed, Plants, Herbaceous, Ferns, Woody, Shrubs, Containerized Trees',
+            'Grow_Type': 'Grown from known seed locations (can track prove"null"ce), Germinated from seed, Grown by clonal dividing',
+            'Specialty': 'Organic, Native Plants',
+            'Address': '1205 S. Buckeye Lane',
+            'Owner_Con': 'Margaret Shea',
+            'Contact_Number': '5024399033',
+            'Contact_Email': 'margaret@dropseednursery.com',
+            'Preferred_Contact': 'Email',
+            'Notes': 'Thanks for doing this!',
+            'Questions and comments': "null",
+            'Zip': 40026.0,
+            'Facebook': "null",
+            'Test': "null",
+            'point': (38.400204605012995,
+                -85.5489178299659,
+                0.0),
+            'Latitude': 38.400204605012995,
+            'Longitude': -85.5489178299659,
+            'elevation': 0.0
+        },
+        'geometry': {
+            'type': 'Point',
+            'coordinates': [
+                -85.5489178299659,
+                38.400204605012995
+            ]
+        }
+    },
+    
+    {
+        'type': 'Feature',
+        'properties': {
+            'Timestamp': '1/23/2020 14: 09: 09',
+            'Nursery_Status': 'New Nursery',
+            'Nursery': 'Country Road Greenhouses',
+            'State_1': 'IL',
+            'Type': 'Wholesale',
+            'Native _%': '100',
+            'Native': 'Yes',
+            'Keep': 'Plants',
+            'Grow_Type': 'Grown from known seed locations (can track prove"null"ce), Germinated from seed',
+            'Specialty': 'Organic, Wetland Restoration, Native Plants',
+            'Address': '19561 Twombly Road',
+            'Owner_Con': 'Bryan and Peggy Redington',
+            'Contact_Number': '9153843311',
+            'Contact_Email': 'crginc@prairieplugs.com',
+            'Preferred_Contact': 'Email',
+            'Notes': "null",
+            'Questions and comments': "null",
+            'Zip': 61068.0,
+            'Facebook': "null",
+            'Test': "null",
+            'point': (40.7749706,
+                -74.4274712,
+                0.0),
+            'Latitude': 40.7749706,
+            'Longitude': -74.4274712,
+            'elevation': 0.0
+        },
+        'geometry': {
+            'type': 'Point',
+            'coordinates': [
+                -74.4274712,
+                40.7749706
+            ]
+        }
+    },
+    
+    {
+        'type': 'Feature',
+        'properties': {
+            'Timestamp': '1/23/2020 17: 08: 32',
+            'Nursery_Status': 'New Nursery',
+            'Nursery': 'GoNativesNow',
+            'State_1': 'OH',
+            'Type': 'Retail, Open to the Public, Appointment Only',
+            'Native _%': '100',
+            'Native': 'Yes',
+            'Keep': 'Seed, Plants, Herbaceous, Woody, Shrubs, Containerized Trees',
+            'Grow_Type': 'Grown from known seed locations (can track prove"null"ce), Germinated from seed',
+            'Specialty': 'Organic, Native Plants',
+            'Address': '107 E. Concord Dr.',
+            'Owner_Con': 'Sue Saari',
+            'Contact_Number': '5135057191',
+            'Contact_Email': 'suesaari@mac.com',
+            'Preferred_Contact': 'Email',
+            'Notes': '2 Open Houses yearly; look for us at the Midwest Native Plant Conference',
+            'Questions and comments': "null",
+            'Zip': 45036.0,
+            'Facebook': "null",
+            'Test': "null",
+            'point': (41.63580618655718,
+                -72.72413350523125,
+                0.0),
+            'Latitude': 41.63580618655718,
+            'Longitude': -72.72413350523125,
+            'elevation': 0.0
+        },
+        'geometry': {
+            'type': 'Point',
+            'coordinates': [
+                -72.72413350523125,
+                41.63580618655718
+            ]
+        }
+    },
+    {
+        'type': 'Feature',
+        'properties': {
+            'Timestamp': '1/23/2020 18: 47: 54',
+            'Nursery_Status': 'Existing Nursery',
+            'Nursery': 'Natural Communities, LLC.',
+            'State_1': 'IL',
+            'Type': 'Wholesale, Retail, Online, Appointment Only',
+            'Native _%': '100',
+            'Native': 'Yes',
+            'Keep': 'Seed, Plants, Herbaceous, Ferns, Woody, Shrubs, Bare Root, Containerized Trees, B&B Trees',
+            'Grow_Type': 'Grown from known seed locations (can track prove"null"ce), Germinated from seed',
+            'Specialty': 'Wetland Restoration, Native Plants',
+            'Address': '19 Circle Dr.',
+            'Owner_Con': 'Nicholas Fuller',
+            'Contact_Number': '2247621501',
+            'Contact_Email': 'natives@naturalcommunities.net',
+            'Preferred_Contact': 'Email',
+            'Notes': 'NATURAL COMMUNITIES BRING NATURE TO LIFE',
+            'Questions and comments': "null",
+            'Zip': 60102.0,
+            'Facebook': "null",
+            'Test': "null",
+            'point': (41.245809,
+                -95.83274663636364,
+                0.0),
+            'Latitude': 41.245809,
+            'Longitude': -95.83274663636364,
+            'elevation': 0.0
+        },
+        'geometry': {
+            'type': 'Point',
+            'coordinates': [
+                -95.83274663636364,
+                41.245809
+            ]
+        }
+    },
+    {
+        'type': 'Feature',
+        'properties': {
+            'Timestamp': '1/23/2020 18: 50: 29',
+            'Nursery_Status': 'Existing Nursery',
+            'Nursery': 'Natural Communities, LLC.',
+            'State_1': 'IL',
+            'Type': 'Wholesale, Retail, Online, Appointment Only',
+            'Native _%': '100',
+            'Native': 'Yes',
+            'Keep': 'Seed, Plants, Herbaceous, Ferns, Woody, Shrubs, Bare Root, Containerized Trees, B&B Trees',
+            'Grow_Type': 'Grown from known seed locations (can track prove"null"ce), Germinated from seed',
+            'Specialty': 'Wetland Restoration, Native Plants',
+            'Address': '3333 Warrenville Rd #200, Lisle, IL ',
+            'Owner_Con': 'Nicholas Fuller',
+            'Contact_Number': '3312481016',
+            'Contact_Email': 'natives@naturalcommunities.net',
+            'Preferred_Contact': 'Email',
+            'Notes': 'NATURAL COMMUNITIES BRING NATURE TO LIFE',
+            'Questions and comments': "null",
+            'Zip': 60532.0,
+            'Facebook': "null",
+            'Test': "null",
+            'point': (41.8081836,
+                -88.0903357,
+                0.0),
+            'Latitude': 41.8081836,
+            'Longitude': -88.0903357,
+            'elevation': 0.0
+        },
+        'geometry': {
+            'type': 'Point',
+            'coordinates': [
+                -88.0903357,
+                41.8081836
+            ]
+        }
+    },
+    {
+        'type': 'Feature',
+        'properties': {
+            'Timestamp': '1/24/2020 13: 26: 57',
+            'Nursery_Status': 'New Nursery',
+            'Nursery': "Mary's Plant Farm & Landscaping",
+            'State_1': 'OH',
+            'Type': 'Retail, Open to the Public, Catalog',
+            'Native _%': '60',
+            'Native': 'Yes',
+            'Keep': 'Seed, Plants, Herbaceous, Ferns, Woody, Shrubs, Bare Root, Containerized Trees, B&B Trees',
+            'Grow_Type': 'Germinated from seed, Grown by clonal dividing',
+            'Specialty': 'Native Plants',
+            'Address': '2410 Lanes Mill Rd',
+            'Owner_Con': 'Sherri Berger, manager',
+            'Contact_Number': '5138940022',
+            'Contact_Email': 'marysplantfarm@zoomtown.com',
+            'Preferred_Contact': 'Email',
+            'Notes': "We are a family owned and operated nursery in our 44th season, growing our plants for hardiness in Ohio soil in our fields, not in a soilless mix in a greenhouse.  During business hours our customers are encouraged to walk Mary's private gardens that are 75 years old, showing mature plants and the locations where they prefer to grow .",
+            'Questions and comments': 'Our complete catalog is found at www.marysplantfarm.com',
+            'Zip': 45013.0,
+            'Facebook': "null",
+            'Test': "null",
+            'point': (39.45098978091304,
+                -84.68346205196627,
+                0.0),
+            'Latitude': 39.45098978091304,
+            'Longitude': -84.68346205196627,
+            'elevation': 0.0
+        },
+        'geometry': {
+            'type': 'Point',
+            'coordinates': [
+                -84.68346205196627,
+                39.45098978091304
+            ]
+        }
+    },
+    {
+        'type': 'Feature',
+        'properties': {
+            'Timestamp': '1/25/2020 12: 34: 24',
+            'Nursery_Status': 'Existing Nursery',
+            'Nursery': 'Stone Silo Prairie Gardens ',
+            'State_1': 'Wi',
+            'Type': 'Wholesale, Retail, Open to the Public, Small/Home Business',
+            'Native _%': '100',
+            'Native': 'No',
+            'Keep': 'Plants, Herbaceous, Ferns',
+            'Grow_Type': 'Germinated from seed, Grown by clonal dividing, Grown from starter plugs',
+            'Specialty': 'Native Plants',
+            'Address': '2325 oak ridge circle ',
+            'Owner_Con': 'Justin Kroening ',
+            'Contact_Number': '9207132879',
+            'Contact_Email': 'kroeningjustin@gmail.com',
+            'Preferred_Contact': 'Phone',
+            'Notes': "null",
+            'Questions and comments': "null",
+            'Zip': 54115.0,
+            'Facebook': "null",
+            'Test': "null",
+            'point': (44.425452,
+                -88.020575,
+                0.0),
+            'Latitude': 44.425452,
+            'Longitude': -88.020575,
+            'elevation': 0.0
+        },
+        'geometry': {
+            'type': 'Point',
+            'coordinates': [
+                -88.020575,
+                44.425452
+            ]
+        }
+    },
+ 
+    {
+        'type': 'Feature',
+        'properties': {
+            'Timestamp': '1/28/2020 9: 47: 56',
+            'Nursery_Status': 'New Nursery',
+            'Nursery': 'Star Farms Native Plants',
+            'State_1': 'OH',
+            'Type': 'Retail, Open to the Public, Appointment Only, Small/Home Business',
+            'Native _%': '100',
+            'Native': 'Yes',
+            'Keep': 'Plants, Herbaceous, Woody, Shrubs, Containerized Trees',
+            'Grow_Type': 'Grown from known seed locations (can track prove"null"ce), Germinated from seed, Grown from starter plugs',
+            'Specialty': 'Native Plants',
+            'Address': '14789 State Route 292, Kenton',
+            'Owner_Con': 'Dave McPheron',
+            'Contact_Number': '937-935-3970',
+            'Contact_Email': 'starfarmsnatives@gmail.com',
+            'Preferred_Contact': 'Phone, Email',
+            'Notes': 'contact information is available at\\nwww.ohionativegrowers.org\\nFaceBook @starfarmsnativeplants',
+            'Questions and comments': "null",
+            'Zip': 43326.0,
+            'Facebook': "null",
+            'Test': "null",
+            'point': (40.61375907839345,
+                -83.59621083913143,
+                0.0),
+            'Latitude': 40.61375907839345,
+            'Longitude': -83.59621083913143,
+            'elevation': 0.0
+        },
+        'geometry': {
+            'type': 'Point',
+            'coordinates': [
+                -83.59621083913143,
+                40.61375907839345
+            ]
+        }
+    },
+    {
+        'type': 'Feature',
+        'properties': {
+            'Timestamp': '2/6/2020 10: 42: 20',
+            'Nursery_Status': 'New Nursery',
+            'Nursery': 'The Common Milkweed',
+            'State_1': 'OH',
+            'Type': 'Retail, Small/Home Business',
+            'Native _%': '100',
+            'Native': 'Yes',
+            'Keep': 'Plants, Herbaceous, Woody, Shrubs, Bare Root',
+            'Grow_Type': 'Germinated from seed',
+            'Specialty': 'Native Plants',
+            'Address': '3344 Township Road 26',
+            'Owner_Con': 'Jennifer Kleinrichert',
+            'Contact_Number': '5056996929',
+            'Contact_Email': 'jennifer@thecommonmilkweed.com',
+            'Preferred_Contact': 'Email',
+            'Notes': "null",
+            'Questions and comments': "null",
+            'Zip': 43315.0,
+            'Facebook': "null",
+            'Test': "null",
+            'point': (40.92519,
+                -84.360347,
+                0.0),
+            'Latitude': 40.92519,
+            'Longitude': -84.360347,
+            'elevation': 0.0
+        },
+        'geometry': {
+            'type': 'Point',
+            'coordinates': [
+                -84.360347,
+                40.92519
+            ]
+        }
+    },
     {
         'type': 'Feature',
         'properties': {
@@ -93,7 +476,7 @@ var geoJson = ({
             'Native _%': '100',
             'Native': 'Yes',
             'Keep': 'Seed, Herbaceous, Ferns, Woody, Shrubs, Bare Root',
-            'Grow_Type': 'Grown from known seed locations (can track provenance), Grown by clonal dividing',
+            'Grow_Type': 'Grown from known seed locations (can track prove"null"ce), Grown by clonal dividing',
             'Specialty': 'Organic, Wetland Restoration, Native Plants',
             'Address': '4101 Fulton Parkway',
             'Owner_Con': 'India Johnson',
